ebf7ce23235b040afbfd361be227d5d51e342804
justinmann <justinmann24@gmail.com>
2023-05-16 16:47:37 -0400

    updates

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import Cookies from 'js-cookie';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'http://localhost:5000/api',
+  baseUrl: 'https://fileupdown.herokuapp.com//api',
   prepareHeaders: (headers) => {
     const aToken = Cookies.get('aToken') ? Cookies.get('aToken') : null;
 
